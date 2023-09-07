58b5ec0ac63e63915e2c8991b32bbc75ee691d2f
ngohuy26 <ngohuyst77@gmail.com>
2023-09-07 15:31:23 +0700

    Thêm Navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './views/navbar';
 
 function App() {
   return (
     <div className="App">
+      <Nav/>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Hello world with React and Ngo Huy!</h1>
