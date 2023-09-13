1b04e6710f798dd85bdcdb91541c3d7b8ae13b3a
ngohuy26 <ngohuyst77@gmail.com>
2023-09-13 16:55:50 +0700

    Test branch

--- src/views/NotFound.js
+++ src/views/NotFound.js
@@ -5,15 +5,15 @@ const NotFoundError = () => {
     const GoHome = () => {
         history.push("/");
     }
-    return(
+    return (
         <div class="d-flex align-items-center justify-content-center ">
             <div class="text-center">
                 <h1 class="display-1 fw-bold">404</h1>
                 <p class="fs-3"> <span class="text-danger">Opps!</span> Page not found.</p>
                 <p class="lead">
-                    The page you’re looking for doesn’t exist.
-                  </p>
-                <a class="btn btn-primary" onClick={() => {GoHome()}}>Go Home</a>
+                    The page you’re looking for doesn’t exist..
+                </p>
+                <a class="btn btn-primary" onClick={() => { GoHome() }}>Go Home</a>
             </div>
         </div>
     )
